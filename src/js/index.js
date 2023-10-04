1bfc74393cb10148704409da182f1ae8a98bc025
Joao2109 <116182164+Joao2109@users.noreply.github.com>
2023-10-04 16:49:22 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,18 @@
 $(function () {
-    const profile = document.querySelector('#profile-pic')
-    const background = document.querySelector('#profile-pic-fixed')
-    const image = document.querySelector('#profile-pic-fixed img')
-    $('#profile-pic-fixed').fadeOut(0)
+    const profile = document.querySelector('#profile-pic');
+    const background = document.querySelector('#profile-pic-fixed');
+    const image = document.querySelector('#profile-pic-fixed img');
+    $('#profile-pic-fixed').fadeOut(0);
+        $('#profile-pic-fixed img').animate({width: '0'},0)
     profile.addEventListener('click', () => {
-        $('#profile-pic-fixed').fadeIn(150)
+        $('#profile-pic-fixed').fadeIn(150);
+        $('#profile-pic-fixed img').animate({width: '90vh'},150)
     })
     background.addEventListener('click', () => {
-        $('#profile-pic-fixed').fadeOut(150)
+        $('#profile-pic-fixed').fadeOut(150);
+        $('#profile-pic-fixed img').animate({width: '0'},150)
     })
     image.addEventListener('click', (e) => {
-        e.stopPropagation()
+        e.stopPropagation();
     })
 })
