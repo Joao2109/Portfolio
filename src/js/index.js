87df8ed606e78dead495d5aa9b6c2525b7632b04
Joao2109 <116182164+Joao2109@users.noreply.github.com>
2023-10-04 16:36:14 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
-const profile = document.querySelector('#profile-pic')
-const background = document.querySelector('#profile-pic-fixed')
-const image = document.querySelector('#profile-pic-fixed img')
-profile.addEventListener('click',()=>{
-    background.classList.add('visible')
-})
-background.addEventListener('click',()=>{
-    background.classList.remove('visible')
-})
-image.addEventListener('click',(e)=>{
-    e.stopPropagation()
+$(function () {
+    const profile = document.querySelector('#profile-pic')
+    const background = document.querySelector('#profile-pic-fixed')
+    const image = document.querySelector('#profile-pic-fixed img')
+    $('#profile-pic-fixed').fadeOut(0)
+    profile.addEventListener('click', () => {
+        $('#profile-pic-fixed').fadeIn(150)
+    })
+    background.addEventListener('click', () => {
+        $('#profile-pic-fixed').fadeOut(150)
+    })
+    image.addEventListener('click', (e) => {
+        e.stopPropagation()
+    })
 })
