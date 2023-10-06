b65388f1dbc621a14c6df01716a1c1ee7ff99b83
Joao2109 <116182164+Joao2109@users.noreply.github.com>
2023-10-06 12:41:47 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,24 @@ $(function () {
     const profile = document.querySelector('#profile-pic');
     const background = document.querySelector('#profile-pic-fixed');
     const image = document.querySelector('#profile-pic-fixed img');
-    $('#profile-pic-fixed').fadeOut(0);
-        $('#profile-pic-fixed img').animate({width: '0'},0)
+    const content = document.querySelectorAll('.content')
     profile.addEventListener('click', () => {
-        $('#profile-pic-fixed').fadeIn(150);
-        $('#profile-pic-fixed img').animate({width: '90vh'},150)
+        $('#profile-pic-fixed').fadeIn(200);
+        $('#profile-pic-fixed img').animate({width: '90vh'},200);
     })
     background.addEventListener('click', () => {
-        $('#profile-pic-fixed').fadeOut(150);
-        $('#profile-pic-fixed img').animate({width: '0'},150)
+        $('#profile-pic-fixed').fadeOut(200);
+        $('#profile-pic-fixed img').animate({width: '0'},200);
     })
     image.addEventListener('click', (e) => {
         e.stopPropagation();
+    })
+    content.forEach((anim, index)=>{
+        anim.addEventListener('mouseover',()=>{
+            $('.content:eq('+index+')').animate({maxWidth: '1200px'},200);
+        })
+        anim.addEventListener('mouseleave',()=>{
+            $('.content:eq('+index+')').animate({maxWidth: '1000px'},200);
+        })
     })
 })
